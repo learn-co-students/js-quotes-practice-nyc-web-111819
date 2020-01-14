0d89e52d3bbcd4e1bb56703f760daed91320d6a6
jasmosez <jas.schaffer@gmail.com>
2020-01-14 17:43:01 -0500

    added edit functionality

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@
 document.addEventListener('DOMContentLoaded', function() {
   quoteList = document.getElementById('quote-list')
   newQuoteForm = document.getElementById('new-quote-form')
+  editMode = false
   
   //
   
   getQuoteList()
   newQuoteForm.addEventListener('submit', submitForm)
   quoteList.addEventListener('click', function(event){
+    event.preventDefault()
+    
     // if click was on a button, run quoteClickHandler
     if (event.target.tagName === "BUTTON") {
       quoteClickHandler(event)
@@ -49,7 +52,8 @@ document.addEventListener('DOMContentLoaded', function() {
     <footer class="blockquote-footer">${quote.author}</footer>
     <br>
     <button class='btn-success likes'>Likes: <span>${likes}</span></button>
-    <button class='btn-danger delete'>Delete</button>
+    <button class='btn-outline-primary edit'>Edit</button>
+    <button class='btn-outline-danger delete'>Delete</button>
     </blockquote>
     `
     
@@ -83,6 +87,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const classList = Array.from(event.target.classList)
     const li = event.target.closest('li')
 
+    if (!!editMode) {
+      //get the li being edited
+      const editLi = quoteList.querySelector(`li[data-id = "${editMode}"]`)
+      const blockquote = editLi.querySelector('blockquote')
+      blockquote.style = ""
+      
+      const form = editLi.querySelector('form#edit-quote-form')
+      
+      // define quote object from editLi before its removal in case it is needed for submit
+      quote = {
+        quote: form["edit-quote"].value,
+        author: form.author.value,
+        id: parseInt(editLi.dataset.id)
+      }
+      
+      form.remove()
+      editMode = false
+    }
+
     if (classList.includes("delete")) {
       console.log("delete")
       deleteQuote(li)
@@ -91,7 +114,15 @@ document.addEventListener('DOMContentLoaded', function() {
       console.log("like")
       likeQuote(li)
 
-    }
+    } else if (classList.includes("edit")) {
+      console.log("edit")
+      editQuote(li)
+
+    } else if (classList.includes("submit")) {
+      console.log("submit")
+      submitEdit(quote)
+
+    } 
   }
 
   function deleteQuote(li){
@@ -133,5 +164,70 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   }
+
+  function editQuote(li){
+    // define quote object from li
+    quote = {
+      quote: li.querySelector('p.mb-0').innerText,
+      author: li.querySelector('footer.blockquote-footer').innerText,
+      id: parseInt(li.dataset.id)
+    }
+
+    // create and insert edit form; hide blockquote
+    const blockquote = li.querySelector('blockquote')
+    blockquote.style.display = "none"
+    
+    const form = editForm(quote)
+    li.insertBefore(form, blockquote)
+    
+    editMode = quote.id
+
+    //the rest is handles by an event handler upon submit
+  }
+
+  function editForm(quote){
+    const form = document.createElement('form')
+    form.id = 'edit-quote-form'
+    form.innerHTML = `
+      Edit Quote
+      <hr>
+      <div>
+        <div class="form-group">
+          <label for="edit-quote">Quote Text</label>
+          <input type="text" class="form-control" id="edit-quote" value="${quote.quote}">
+        </div>
+        <div class="form-group">
+          <label for="Author">Author</label>
+          <input type="text" class="form-control" id="author" value="${quote.author}">
+        </div>
+        <button type="submit" class="btn btn-primary submit">Submit</button>
+      </div>
+      <hr>
+    `
+    return form
+
+  }
+
+  function submitEdit(editedQuote){
+
+    configObj = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accepts": "application/json"
+      },
+      body: JSON.stringify(editedQuote)
+    }
+
+    fetch("http://localhost:3000/quotes/" + quote.id, configObj)
+    .then(resp => resp.json())
+    .then(quote => {
+      li = quoteList.querySelector(`li[data-id = "${quote.id}"`)
+      li.querySelector(`p.mb-0`).innerText = quote.quote
+      li.querySelector(`footer.blockquote-footer`).innerText = quote.author
+    })
+  }
+
+
 
 })
